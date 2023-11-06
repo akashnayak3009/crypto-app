707497e12a1f39c6d7268c1603510a29bb352da5
Akash kumar Nayak <akashnayak3009@gmail.com>
2023-11-06 18:44:45 +0530

    ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Exchanges from "./components/Exchanges";
+import Home from "./components/Home";
+import Coins from "./components/Coins";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
-        <Route/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/coins" element={<Coins/>}/>
+        <Route path="/exchanges" element={<Exchanges/>}/>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
